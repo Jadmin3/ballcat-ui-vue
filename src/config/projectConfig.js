371d88f45790b80d34b7a90e81730e1ebba7ab3b
Jadmin <1243678726@qq.com>
2021-12-20 21:38:24 +0800

    fix:typo

--- src/config/projectConfig.js
+++ src/config/projectConfig.js
@@ -1,8 +1,8 @@
 module.exports = {
   // 项目标题
-  projectTitle: 'Ball Cat',
+  projectTitle: 'Samart Cat',
   // 项目描述
-  projectDesc: 'Ball Cat 一个简单的项目启动脚手架',
+  projectDesc: 'Samart Cat 一个简单的项目启动脚手架',
   // Vue ls 配置
   storageOptions: {
     namespace: 'ballcat/', // key prefix
